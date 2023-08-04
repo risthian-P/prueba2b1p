5675c056fb608c6cd0d3301367c1e6bcdeb059ef
Cristhian Panora <cristhian.panora@gmail.com>
2023-08-04 11:33:56 -0500

    nuevos cambios

--- src/formulario.java
+++ src/formulario.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class formulario {
+import javax.swing.*;
+
+public class formulario {
+    private JPanel rootPanel;
+    private JTextField txtcodig;
+    private JTextField txtcedula;
+    private JTextField txtnombre;
+    private JTextField txtfecha;
+    private JComboBox Signo;
+    private JButton buscarPorCodigoButton;
+    private JButton buscarPorNombreButton;
+    private JButton buscarPorSignoButton;
+    private JButton borrarElPresenteRegistroButton;
+    private JButton actualizarElPresenteRegistroButton;
+    private JButton ingresarElPresenteRegistroButton;
+    private JButton limpiarFormularioButton;
+
+    private formulario(){
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("formulario");
+        frame.setContentPane(new formulario().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
